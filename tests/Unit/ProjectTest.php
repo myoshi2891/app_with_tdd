929d5a776842e6365f796437ec2d28c7881b76de
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-10-09 13:34:22 +0900

    Model Tests

---
+++ tests/Unit/ProjectTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Models\Project;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+class ProjectTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+
+    /** @test */
+    public function it_has_a_path()
+    {
+        $project = Project::factory()->create();
+
+        $this->assertEquals('/projects/' . $project->id, $project->path());
+    }
+}
