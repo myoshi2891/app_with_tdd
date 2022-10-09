2a38a9f870a3ff4db6d699822ce84956f3785a9f
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-10-09 12:55:12 +0900

    Testing Request Validation

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Project;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     * @var string
+     */
+    protected $model = Project::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+        ];
+    }
+}
