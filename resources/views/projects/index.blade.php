9dcce04d7195c2546703e1d70053985253aba11a
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-10-10 10:48:49 +0900

    Grid Spacing and Card Tweaks

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,15 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex items-center mb-3">
-        <a href="/projects/create">New Project</a>
+    <div class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-center w-full">
+            <h2 class="text-gray-400 text-sm font-normal">My Projects</h2>
+            <a href="/projects/create" class="bg-blue-400 text-white text-sm no-underline rounded-lg shadow-md py-2 px-5">New
+                Project</a>
+        </div>
     </div>
 
-    <div class="flex">
+    <div class="lg:flex lg:flex-wrap -mx-3">
         @forelse ($projects as $project)
-            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px">
-                <h3 class="font-normal text-xl py-6">{{ $project->title }}</h3>
-                <div class="text-grey">{{ Str::limit($project->description, 100) }}</div>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
+                    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-400 pl-4">
+                        <a href="{{ $project->path() }}">{{ $project->title }}</a>
+                    </h3>
+                    <div class="text-gray-400">{{ Str::limit($project->description, 100) }}</div>
+                </div>
             </div>
         @empty
             <div>No projects yet.</div>
