85740f8aee784f10182cded1a519e192f1133edc
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-10-10 16:20:29 +0900

    Task UI Updates

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -20,12 +20,20 @@
                     @foreach ($project->tasks as $task)
                         <div class="bg-white p-5 rounded-lg shadow mb-3">{{ $task->body }}</div>
                     @endforeach
-                </div>
-                <div>
-                    <h2 class="text-lg text-gray-400 font-normal mb-3">General Notes</h2>
 
-                    <textarea class="bg-white rounded-lg shadow min-h-full w-full p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    <div class="bg-white p-5 rounded-lg shadow mb-3">
+                        <form action="{{ $project->path() . '/tasks' }}" method="POST">
+                            @csrf
+                            <input placeholder="Add a new task." class="w-full" name="body">
+                        </form>
+
+                    </div>
+                    <div>
+                        <h2 class="text-lg text-gray-400 font-normal mb-3">General Notes</h2>
+
+                        <textarea class="bg-white rounded-lg shadow min-h-full w-full p-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                         Cupiditate, natus.</textarea>
+                    </div>
                 </div>
             </div>
             <div class="lg:w-1/4 px-3">
