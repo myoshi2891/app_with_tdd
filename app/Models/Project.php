a57edd6f141428f188b663d42e5aa93a273452b9
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-10-16 11:15:09 +0900

    Recoding Model Changes

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use App\Models\Task;
 use App\Models\User;
+use App\Models\Activity;
+use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -12,6 +14,8 @@ class Project extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public $old = [];
 
     public function path()
     {
@@ -35,7 +39,21 @@ class Project extends Model
 
     public function recordActivity($description)
     {
-        $this->activity()->create(compact('description'));
+
+        $this->activity()->create([
+            'description' => $description,
+            'changes' => $this->activityChanges($description)
+        ]);
+    }
+
+    protected function activityChanges($description)
+    {
+        if ($description == 'updated') {
+            return [
+                'before' => Arr::except(array_diff($this->old, $this->getAttributes()), 'updated_at'),
+                'after' => Arr::except($this->getChanges(), 'updated_at')
+            ];
+        }
     }
 
     public function activity()
