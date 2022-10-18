d1df299abdd4935a51b4743bb1fea8f8e9564e4d
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-10-18 21:40:39 +0900

    Authorized Users Can Delete Projects

--- app/Models/RecordsActivity.php
+++ app/Models/RecordsActivity.php
@@ -34,7 +34,7 @@ trait RecordsActivity
     if (isset(static::$recordableEvents)) {
       return static::$recordableEvents;
     }
-    return ['created', 'updated', 'deleted'];
+    return ['created', 'updated'];
   }
 
   public function recordActivity($description)
--- resources/views/projects/activity/updated_project.blade.php
+++ resources/views/projects/activity/updated_project.blade.php
@@ -1,5 +1,5 @@
 @if (count($activity->changes['after']) == 1)
-    {{ $activity->user->name }} uypdated the {{ key($activity->changes['after']) }}
+    {{ $activity->user->name }} updated the {{ key($activity->changes['after']) }}
 @else
     {{ $activity->user->name }} updated the project
 @endif
